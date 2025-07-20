c95d68a4a5d525dcda1b238c813c82a777bfc939
xmingc <chenxm35@gmail.com>
2025-07-20 22:43:07 +0800

    Update: "dev_env.zsh"

--- dev_env.zsh
+++ dev_env.zsh
@@ -1,7 +1,7 @@
 # Manage dev environments.
 
 # Function to add to PATH if the directory exists
-add_to_path() {
+_prepend_to_path() {
   local dir=$1
   if [ -d "$dir" ]; then
     export PATH="$dir:$PATH"
@@ -22,33 +22,33 @@ _init_go_env() {
 _init_pyenv() {
   if [ -d "$HOME/.pyenv" ]; then
     export PYENV_ROOT="$HOME/.pyenv"
-    add_to_path "$PYENV_ROOT/bin"
+    _prepend_to_path "$PYENV_ROOT/bin"
     eval "$(pyenv init -)"
   fi
 }
 
 # Function to initialize jenv environment
 _init_jenv() {
-  add_to_path "$HOME/.jenv/bin"
+  _prepend_to_path "$HOME/.jenv/bin"
   eval "$(jenv init -)"
   # jenv enable-plugin export > /dev/null
 }
 
 # Function to initialize Haskell environment
 _init_haskell_env() {
-  add_to_path "$HOME/.ghcup/bin"
-  add_to_path "$HOME/.cabal/bin"
+  _prepend_to_path "$HOME/.ghcup/bin"
+  _prepend_to_path "$HOME/.cabal/bin"
 }
 
 # Function to initialize Lisp environment
 _init_lisp_env() {
   # Roswell
-  add_to_path "$HOME/.roswell/bin"
+  _prepend_to_path "$HOME/.roswell/bin"
   # Customize quicklisp home
   export QUICKLISP_HOME=${HOME}/quicklisp
   # Allegro CL
   local ACL_HOME=${HOME}/.local/share/acl
-  add_to_path "$ACL_HOME"
+  _prepend_to_path "$ACL_HOME"
   # Start with rlwrap
   alias sbcl="rlwrap -f $HOME/.config/rlwrap/lisp_completions --remember sbcl"
   alias alisp="rlwrap -f $HOME/.config/rlwrap/lisp_completions --remember alisp"
