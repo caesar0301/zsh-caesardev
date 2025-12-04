be89f390f6fb5496a7db7b1184b7e347fa42f006
Xiaming Chen <chenxm35@gmail.com>
2025-12-04 10:56:02 +0800

    Configure Homebrew Git remote and settings
    Set custom Git remote for Homebrew and disable auto-update.

--- env-vars.zsh
+++ env-vars.zsh
@@ -1,2 +1,7 @@
+# Set non-default Git remote for Homebrew/homebrew-core.
+export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.ustc.edu.cn/homebrew-core.git"
+export HOMEBREW_NO_AUTO_UPDATE=1
+
 export RLWRAP_HOME=${HOME}/.config/rlwrap
 export PATH=$HOME/.npm-global/bin:$PATH
+export PATH=$HOME/go/bin:$PATH
